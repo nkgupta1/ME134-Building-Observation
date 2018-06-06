69747b6a9fbdf656e12c37d7c4c31965c73cc209
Nikhil Gupta <nikhil97gupta@gmail.com>
2018-06-05 20:22:34 -0700

    Working operation to move around
    Also add the rqt_graph for the bebop_driver

--- src/operation.py
+++ src/operation.py
@@ -4,44 +4,77 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Empty
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
 
-def takeoff():
-    takeoff = rospy.Publisher('/bebop/takeoff', Empty, queue_size=10)
-    rospy.sleep(1)
+import time
+
+
+def takeoff(takeoff_t):
+    print "Taking off"
     takeoff_msg = Empty()
-    takeoff.publish(takeoff_msg)
-
-def land():
-    land = rospy.Publisher('/bebop/land', Empty, queue_size=10)
+    takeoff_t.publish(takeoff_msg)
     rospy.sleep(1)
-    land_msg = Empty()
-    land.publish(land_msg)
-    
 
-def talker():
+def land(land_t):
+    print "Landing"
+    land_msg = Empty()
+    land_t.publish(land_msg)
+    rospy.sleep(1)
+    
+def empty_twist():
+    twist = Twist()
+
+    twist.linear.x = 0
+    twist.linear.y = 0
+    twist.linear.z = 0
+
+    twist.angular.x = 0
+    twist.angular.y = 0
+    twist.angular.z = 0
+
+    return twist
+
+def controller():
 
     rospy.init_node('simple_test')
+
+    print "creating topics"
+    takeoff_t = rospy.Publisher('/bebop/takeoff', Empty, queue_size=10)
+    rospy.sleep(1)
+    cmd_vel_t = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size=10)
+    rospy.sleep(1)
+    land_t = rospy.Publisher('/bebop/land', Empty, queue_size=10)
+    rospy.sleep(1)
     
-    takeoff()
-    # TODO: move in dead reckoning
     rospy.sleep(5)
-    land()
+    
+    takeoff(takeoff_t)
+
+    # Let the drone actually take off
+    print "Pausing"
+    rospy.sleep(5)
+
+    # need to refresh the command on the periodic interval
+    rate = rospy.Rate(10) # 10 Hz
+    t0 = time.time()
 
     
-    # rate = rospy.Rate(10) # 10hz
-    # while not rospy.is_shutdown():
-    #     goal = PoseStamped()
-    #     goal.header.frame_id = "/map"
-    #     goal.header.stamp = rospy.Time.now()
-    #     goal.pose.position.x = 3.0
-    #     goal.pose.position.y = 1.0
-    #     goal.pose.position.z = 0.0
-    #     goal.pose.orientation.w = 1.0
-    #     pub.publish(goal)
-    #     rate.sleep()
+    print "Moving up"
+    while (time.time() - t0 < 2):
+        twist = empty_twist()
+        twist.linear.z = 1
+        
+        cmd_vel_t.publish(twist)
+        rate.sleep()
 
+
+
+    rospy.sleep(2)
+    land(land_t)
+
+    
 if __name__ == '__main__':
     try:
-        talker()
+        controller()
     except rospy.ROSInterruptException:
         pass
