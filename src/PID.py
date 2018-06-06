aedfac77b1c096f621886406d8ff347254159d80
Nikhil Gupta <nikhil97gupta@gmail.com>
2018-06-06 00:19:06 -0700

    Fix identation bug

--- src/PID.py
+++ src/PID.py
@@ -4,85 +4,86 @@ import time
 
 # Sourced from http://ivmech.github.io/ivPID/
 class PID:
-	"""PID Controller"""
+    """PID Controller"""
 
-	def __init__(self, SetPoint=0.0, P=0.2, I=0.0, D=0.0):
-		self.Kp = P
-		self.Ki = I
-		self.Kd = D
+    def __init__(self, SetPoint=0.0, P=0.2, I=0.0, D=0.0):
+        self.clear()
 
-		self.SetPoint = SetPoint
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
 
-		self.sample_time = 0.00
-		self.current_time = time.time()
-		self.last_time = self.current_time
-		self.clear()
+        self.SetPoint = SetPoint
 
-	def clear(self):
-		"""Clears PID computations and coefficients"""
-		self.SetPoint = 0.0
-		self.PTerm = 0.0
-		self.ITerm = 0.0
-		self.DTerm = 0.0
-		self.last_error = 0.0
+        self.sample_time = 0.00
+        self.current_time = time.time()
+        self.last_time = self.current_time
 
-		# Windup Guard
-		self.int_error = 0.0
-		self.windup_guard = 20.0
-		self.output = 0.0
+    def clear(self):
+        """Clears PID computations and coefficients"""
+        self.SetPoint = 0.0
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.last_error = 0.0
 
-	def update(self, feedback_value):
-		"""Calculates PID value for given reference feedback
-		math: u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}"""
-		error = self.SetPoint - feedback_value
-		self.current_time = time.time()
-		delta_time = self.current_time - self.last_time
-		delta_error = error - self.last_error
+        # Windup Guard
+        self.int_error = 0.0
+        self.windup_guard = 20.0
+        self.output = 0.0
 
-		if (delta_time >= self.sample_time):
-			self.PTerm = self.Kp * error
-			self.ITerm += error * delta_time
+    def update(self, feedback_value):
+        """Calculates PID value for given reference feedback
+        math: u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}"""
+        error = self.SetPoint - feedback_value
+        self.current_time = time.time()
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
 
-			if (self.ITerm < -self.windup_guard):
-				self.ITerm = -self.windup_guard
-			elif (self.ITerm > self.windup_guard):
-				self.ITerm = self.windup_guard
+        if (delta_time >= self.sample_time):
+            self.PTerm = self.Kp * error
+            self.ITerm += error * delta_time
 
-			self.DTerm = 0.0
-			if delta_time > 0:
-				self.DTerm = delta_error / delta_time
+            if (self.ITerm < -self.windup_guard):
+                self.ITerm = -self.windup_guard
+            elif (self.ITerm > self.windup_guard):
+                self.ITerm = self.windup_guard
 
-			# Remember last time and last error for next calculation
-			self.last_time = self.current_time
-			self.last_error = error
+            self.DTerm = 0.0
+            if delta_time > 0:
+                self.DTerm = delta_error / delta_time
 
-			self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+            # Remember last time and last error for next calculation
+            self.last_time = self.current_time
+            self.last_error = error
 
-		def setKp(self, proportional_gain):
-			"""Determines how aggresively the PID reacts to the currrent error with setting Proportional Gain"""
-			self.Kp = proportional_gain
+            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
 
-    	def setKi(self, integral_gain):
-        	"""Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
-        	self.Ki = integral_gain
+        def setKp(self, proportional_gain):
+            """Determines how aggresively the PID reacts to the currrent error with setting Proportional Gain"""
+            self.Kp = proportional_gain
 
-    	def setKd(self, derivative_gain):
-        	"""Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
-        	self.Kd = derivative_gain
+        def setKi(self, integral_gain):
+            """Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
+            self.Ki = integral_gain
+
+        def setKd(self, derivative_gain):
+            """Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
+            self.Kd = derivative_gain
 
         def setWindup(self, windup):
-			"""Integral windup, also known as integrator windup or reset windup,
-	        refers to the situation in a PID feedback controller where
-	        a large change in setpoint occurs (say a positive change)
-	        and the integral terms accumulates a significant error
-	        during the rise (windup), thus overshooting and continuing
-	        to increase as this accumulated error is unwound
-	        (offset by errors in the other direction).
-	        The specific problem is the excess overshooting.
-	        """
-	        self.windup_guard = windup
+            """Integral windup, also known as integrator windup or reset windup,
+            refers to the situation in a PID feedback controller where
+            a large change in setpoint occurs (say a positive change)
+            and the integral terms accumulates a significant error
+            during the rise (windup), thus overshooting and continuing
+            to increase as this accumulated error is unwound
+            (offset by errors in the other direction).
+            The specific problem is the excess overshooting.
+            """
+            self.windup_guard = windup
 
-	    def setSampleTime(self, sample_time):
-	    	"""PID that should be updated at a regular interval. Based on a predetermined sample time, the
-	    	PID decides if it should compute or return immediately."""
-	    	self.sample_time = sample_time
+        def setSampleTime(self, sample_time):
+            """PID that should be updated at a regular interval. Based on a predetermined sample time, the
+            PID decides if it should compute or return immediately."""
+            self.sample_time = sample_time
