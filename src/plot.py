443e3c1705e4c2e762113071e4762c6e8cd76d0e
Nikhil Gupta <nikhil97gupta@gmail.com>
2018-06-08 12:40:04 -0700

    add error checking and saving to plotting code

--- src/plot.py
+++ src/plot.py
@@ -6,6 +6,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np 
+import time
 np.random.seed(444)
 
 odom_data = './bebop.csv' # Set the file name to be read
@@ -16,7 +17,10 @@ t_odom = []
 x_odom = []
 y_odom = []
 z_odom = []
-goals = []
+
+x_goal = []
+y_goal = []
+z_goal = []
 
 data = []
 
@@ -24,17 +28,29 @@ data = []
 with open(odom_data, 'r+') as cvsfile:
     reader = csv.reader(cvsfile)
     for line in reader:
+        if len(line) != 5:
+            continue
         entry = []
         for elt in line:
-            entry.append(float(elt))
-        data.append(entry)
+            try:
+                entry.append(float(elt))
+            except Exception as e:
+                entry = []
+                break
+        if entry != []:
+            data.append(entry)
 
 # Assign the values in data to either goals or odom
 for x in data:
     if x[0] == 0:
-        pass
-        # goals.append(x)
+        if len(x) != 5:
+            continue
+        x_goal.append(x[2])
+        y_goal.append(x[3])
+        z_goal.append(x[4])
     elif x[0] == 1:
+        if len(x) != 5:
+            continue
         odom.append(x)
         t_odom.append(x[1])
         x_odom.append(x[2])
@@ -46,14 +62,21 @@ for x in data:
 # Plot the 1D odom data
 fig, axes = plt.subplots(2,2)
 
-axes[0,0].plot(t_odom, x_odom, 'o-')
+axes[0,0].plot(t_odom, x_odom, '-')
 axes[0,0].set(xlabel='t', ylabel='x')
+axes[0,0].axhline(x_goal[0])
 
-axes[0,1].plot(t_odom, y_odom, 'o-')
-axes[0,1].set(xlabel='t', ylabel='x')
+axes[0,1].plot(t_odom, y_odom, '-')
+axes[0,1].set(xlabel='t', ylabel='y')
+axes[0,1].axhline(y_goal[0])
 
-axes[1,0].plot(t_odom, z_odom, 'o-')
-axes[1,0].set(xlabel='t', ylabel='x')
+axes[1,0].plot(t_odom, z_odom, '-')
+axes[1,0].set(xlabel='t', ylabel='z')
+axes[1,0].axhline(z_goal[0])
 
+t = time.time()
+plt.savefig('graphs/%5d'%t)
 plt.show()
+
+print '%5d.png'%t
 
